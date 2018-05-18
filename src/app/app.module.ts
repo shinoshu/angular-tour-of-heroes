ef35e35aa157b2d1285b972ae9bf15cd16607c1e
Shuma Shinozaki <shu.shinozaki@gmail.com>
2018-05-18 18:50:57 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 
 @NgModule({
   imports: [
@@ -37,6 +39,7 @@ import { MessagesComponent } from './messages/messages.component';
     MessagesComponent,
     HeroSearchComponent
   ],
+  providers: [HeroService, MessageService],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
